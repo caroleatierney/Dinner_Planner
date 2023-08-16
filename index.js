2395b04b8b28265dc99beecfe312aee6cf46d6c4
carole tierney <caroleatierney@gmail.com>
2023-08-16 18:02:12 -0400

    delete buttons added and not working

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function addMeal() {
     const table = document.getElementById("dinnerPlannerTable");
 
     let row = table.insertRow(tableRowCount);
-    row.setAttribute('id', `item-${id++}`);
+    row.setAttribute('id', `item-${id}`);
     row.insertCell(0).innerHTML = document.getElementById("dayOfWeek").value;
     document.getElementById("dinnerPlannerTable").rows[id].cells.item(0).setAttribute("style", "color: #e76f51;");
     row.insertCell(1).innerHTML = document.getElementById("date").value;
@@ -16,6 +16,32 @@ function addMeal() {
     row.insertCell(3).innerHTML = document.getElementById("veggie").value;
     row.insertCell(4).innerHTML = document.getElementById("starch").value;
     row.insertCell(5).innerHTML = document.getElementById("howMany").value;
+
+    // let actions = row.insertCell(6)  // set a variable to the 6th cell that will hold the button
+    // actions.appendChild(deleteButton(id++))  // append the button and increment the id
+    row.insertCell(6).appendChild(deleteButton(id++)) 
+
+    alert(`'Id', ${id}`);
 }
 
 // Allow user to delete rows of a table dynamically
+function deleteButton(id) {
+    let btn = document.createElement('button');
+    btn.className = 'btn btn-primary'
+    btn.id = id;
+    btn.innerHTML = 'Delete';
+    btn.onclick = () => {
+        alert(`Deleted row with id: item-${id}`);
+        let rowToDelete = document.getElementById(`item-${id}`);
+        console.log(rowToDelete)
+        rowToDelete.parentNode.removeChild(rowToDelete);
+    };
+    return btn;
+}
+
+function deleteTest(id) {
+    alert(`Deleted row with id: ${id}`);
+    let rowToDelete = document.getElementById(`${id}`);
+    console.log(rowToDelete)
+    rowToDelete.parentNode.removeChild(rowToDelete);
+}
