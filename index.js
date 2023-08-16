18b34957a8071983c32b6f20e704080fc8c95905
carole tierney <caroleatierney@gmail.com>
2023-08-16 15:20:21 -0400

    got color to change

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-
+// Allow user to add rows to a table dynamically
 
 function addMeal() {
-
-    // var oRows = document.getElementById('dinnerPlannerTable').getElementsByTagName('tr');
-    // var iRowCount = oRows.length;
-    // alert('Your table has ' + iRowCount + ' rows.');
-    
-    var tableRowCount = document.getElementById('dinnerPlannerTable').getElementsByTagName('tr').length;
-    alert('Your table has ' + tableRowowCount + ' rows.');
+    var rows = document.getElementById('dinnerPlannerTable').getElementsByTagName('tr');
+    var tableRowCount = rows.length;
+    let id = tableRowCount-1;
 
     const table = document.getElementById("dinnerPlannerTable");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-    var cell6 = row.insertCell(5);
-    cell1.innerHTML = "new cell 1";
-    cell2.innerHTML = "new cell 2";
-    cell3.innerHTML = "new cell 3";
-    cell4.innerHTML = "new cell 4";
-    cell5.innerHTML = "new cell 5";
-    cell6.innerHTML = "new cell 6";
+
+    let row = table.insertRow(tableRowCount);
+    row.setAttribute('id', `item-${id++}`);
+    row.insertCell(0).innerHTML = document.getElementById("dayOfWeek").value;
+    document.getElementById("dinnerPlannerTable").rows[id].cells.item(0).setAttribute("style", "color: #e76f51;");
+    row.insertCell(1).innerHTML = document.getElementById("date").value;
+    row.insertCell(2).innerHTML = document.getElementById("protein").value;
+    row.insertCell(3).innerHTML = document.getElementById("veggie").value;
+    row.insertCell(4).innerHTML = document.getElementById("starch").value;
+    row.insertCell(5).innerHTML = document.getElementById("howMany").value;
 }
+
+// Allow user to delete rows of a table dynamically
