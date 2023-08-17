48a1ac0d2800d25ecdf41bd6e74bbc457acbd752
carole tierney <caroleatierney@gmail.com>
2023-08-17 12:35:49 -0400

    delete buttons for new work, font size and color changed

--- index.js
+++ index.js
@@ -3,25 +3,33 @@
 function addMeal() {
     var rows = document.getElementById('dinnerPlannerTable').getElementsByTagName('tr');
     var tableRowCount = rows.length;
-    let id = tableRowCount-1;
+    let id = tableRowCount;
 
     const table = document.getElementById("dinnerPlannerTable");
 
     let row = table.insertRow(tableRowCount);
     row.setAttribute('id', `item-${id}`);
-    row.insertCell(0).innerHTML = document.getElementById("dayOfWeek").value;
-    document.getElementById("dinnerPlannerTable").rows[id].cells.item(0).setAttribute("style", "color: #e76f51;");
+
+    let cell1 = row.insertCell(0)
+    cell1.innerHTML = document.getElementById("dayOfWeek").value;
+    cell1.style.color = "#e76f51";
+    cell1.style.fontWeight = "bold"
+
     row.insertCell(1).innerHTML = document.getElementById("date").value;
     row.insertCell(2).innerHTML = document.getElementById("protein").value;
     row.insertCell(3).innerHTML = document.getElementById("veggie").value;
     row.insertCell(4).innerHTML = document.getElementById("starch").value;
     row.insertCell(5).innerHTML = document.getElementById("howMany").value;
+    row.insertCell(6).appendChild(deleteButton(id))
 
-    // let actions = row.insertCell(6)  // set a variable to the 6th cell that will hold the button
-    // actions.appendChild(deleteButton(id++))  // append the button and increment the id
-    row.insertCell(6).appendChild(deleteButton(id++)) 
+    document.getElementById('dayOfWeek').value = '';
+    document.getElementById('date').value = '';
+    document.getElementById('protein').value = '';
+    document.getElementById('veggie').value = '';
+    document.getElementById('starch').value = '';
+    document.getElementById('howMany').value = '';
 
-    alert(`'Id', ${id}`);
+    // alert(`'Id added:', ${id}`); // display id just added
 }
 
 // Allow user to delete rows of a table dynamically
@@ -31,7 +39,7 @@ function deleteButton(id) {
     btn.id = id;
     btn.innerHTML = 'Delete';
     btn.onclick = () => {
-        alert(`Deleted row with id: item-${id}`);
+        // alert(`Deleted row with id: item-${id}`); // display item id of row being deleted
         let rowToDelete = document.getElementById(`item-${id}`);
         console.log(rowToDelete)
         rowToDelete.parentNode.removeChild(rowToDelete);
